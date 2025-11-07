00a2280fb7f5b030400a748563e72126f4973b18
prajwalnehe <prajwalnehe383@gmail.com>
2025-11-07 10:39:55 +0530

    add edit option

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import auth from '../middleware/auth.js';
 import adminOnly from '../middleware/admin.js';
-import { createProduct, adminListProducts, deleteProductById, adminListOrders, adminStats, adminListAddresses } from '../controllers/admin.controller.js';
+import { createProduct, adminListProducts, deleteProductById, adminListOrders, adminStats, adminListAddresses, updateProduct } from '../controllers/admin.controller.js';
 
 const router = Router();
 
 // Products
 router.post('/products', auth, adminOnly, createProduct);
 router.get('/products', auth, adminOnly, adminListProducts);
+router.patch('/products/:id', auth, adminOnly, updateProduct);
 router.delete('/products/:id', auth, adminOnly, deleteProductById);
 
 // Orders
